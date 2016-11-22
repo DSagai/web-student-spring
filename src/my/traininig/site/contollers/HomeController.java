7ab605d4696110cd5d19a46003503409bb3c51bb
Dmytro Sagai <accn1@yandex.ru>
2016-11-22 18:03:27 +0200

    update HomeController response type

--- src/my/traininig/site/contollers/HomeController.java
+++ src/my/traininig/site/contollers/HomeController.java
@@ -1,6 +1,6 @@
 package my.traininig.site.contollers;
 
-import java.util.List;
+import java.util.*;
 
 import javax.inject.Named;
 import javax.servlet.http.HttpServletRequest;
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import my.traininig.site.model.*;
 
@@ -30,9 +31,12 @@ public class HomeController {
 	}
 	
 	@RequestMapping("/")
-	public String getStudentList(HttpServletRequest request){
+	public ModelAndView getStudentList(){
 		List<Student> students = studentDbUtil.getStudents();
-		request.setAttribute("STUDENT_LIST", students);
-		return "list-students";
+		ModelAndView mv = new ModelAndView("list-students");
+		Map<String,Object> model = mv.getModel(); 
+		model.put("STUDENT_LIST", students);
+		
+		return mv;
 	}
 }
